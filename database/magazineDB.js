34111d2a0fa152d45209f9935051bb9770c96ce9
kun.xue <kun.xue@buzzinate.com>
2014-10-04 22:42:47 +0800

    add magazine api

--- database/magazineDB.js
+++ database/magazineDB.js
@@ -25,10 +25,39 @@ magazineDB.listMagazines = function (callback) {
 };
 
 /**
- * get magazine item info;
+ * get magazine item info by id;
+ * @param id --id of periodical.
  * @param callback
  */
-magazineDB.listItems = function (callback) {
-    database.query('select ', callback);
-}
+magazineDB.listArticles = function (id, callback) {
+    database.query('select a.id, a.name as title, a.creationdate as \'datetime\'' +
+        ' from article a, issue b' +
+        ' where a.issueId = b.id and b.id=\'' + id + "\'" +
+        ' order by creationdate desc', callback);
+};
+
+/**
+ * get id of next periodical by current id;
+ * @param id
+ * @param callback
+ */
+magazineDB.getNextMagazineId = function (id, callback) {
+    database.query('select top 1 id' +
+        ' from issue' +
+        ' where id > \'' + id + '\' and periodicalId in' +
+        ' (select periodicalId from issue where id = \'' + id + '\') order by id asc', callback);
+};
+
+/**
+ * get id of previous periodical by current id;
+ * @param id
+ * @param callback
+ */
+magazineDB.getPreviousMagazineId = function (id, callback) {
+    database.query('select top 1 id' +
+        ' from issue' +
+        ' where id < \'' + id + '\' and periodicalId in' +
+        ' (select periodicalId from issue where id = \'' + id + '\') order by id desc', callback);
+};
+
 module.exports = magazineDB;
--- routes/magazine.js
+++ routes/magazine.js
@@ -2,15 +2,104 @@
  * Created by kun
  */
 var express = require('express');
+var magazineDB = require('../database/magazineDB');
+var validator = require('validator');
+var async = require('async');
 var router = express.Router();
 
+var returnArticles = function (err, recordSet) {
+    if (!err) {
+        var result = [];
+        for (var i = 0; i < recordSet.length; i++) {
+            result[i] = recordSet[i];
+        }
+        res.json(200, {result: ture, data: result});
+    } else {
+        res.json(200, {result: false, data: "发生异常"});
+    }
+};
+
+var checkParam = function (req, res, next) {
+    var magId = req.param("magId");
+    if (!validator.isNumeric(magId)) {
+        res.json(400, {result: false, data: "invalid param magId"});
+    } else {
+        next();
+    }
+}
+
+/**
+ * 获取最新杂志列表
+ */
 router.get('/list', function (req, res) {
-    //TODO
-    res.json(200, {result: ture});
+    magazineDB.listMagazines(function (err, recordSet) {
+        if (!err) {
+            var result = [];
+            for (var i = 0; i < recordSet.length; i++) {
+                result[i] = recordSet[i];
+            }
+            res.json(200, {result: ture, data: result});
+        } else {
+            res.json(200, {result: false, data: "发生异常"});
+        }
+    });
 });
 
-router.get('/menus', function (req, res) {
-    //TODO
+
+/**
+ * 获取杂志目录信息
+ */
+router.get('/articles', checkParam, function (req, res) {
+    var magId = req.param("magId");
+    magazineDB.listArticles(magId, returnArticles);
+});
+
+/**
+ * 获取下一期杂志目录信息
+ */
+router.get('/nextMag', function (req, res) {
+    var magId = req.param("magId");
+    async.waterfall(
+        function (callback) {
+            magazineDB.getNextMagazineId(magId, function (err, recordSet) {
+                if (!err && recordSet.length > 0) {
+                    var nextId = recordSet[0].id;
+                    callback(null, nextId);
+                } else {
+                    res.json(200, {result: false, data: (err ? "get next magazine failed!" : [])});
+                    callback("no next magazine", null);
+                }
+            });
+
+        },
+        function (nextId, callback) {
+            magazineDB.listArticles(nextId, returnArticles);
+        }
+    );
+});
+
+/**
+ * 获取上一期杂志目录信息
+ */
+router.get('/prevMag', function (req, res) {
+    var magId = req.param("magId");
+    async.waterfall(
+        function (callback) {
+            magazineDB.getPreviousMagazineId(magId, function (err, recordSet) {
+                if (!err && recordSet.length > 0) {
+                    var nextId = recordSet[0].id;
+                    callback(null, nextId);
+                } else {
+                    res.json(200, {result: false, data: (err ? "get previous magazine failed!" : [])});
+                    callback("no previous magazine", null);
+                }
+            });
+
+        },
+        function (nextId, callback) {
+            magazineDB.listArticles(nextId, returnArticles);
+        }
+    );
 });
 
 module.exports = router;
