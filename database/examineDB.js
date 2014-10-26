cfef012eafc0f06da77913eb1be2a92fe159308e
kun.xue <kun.xue@buzzinate.com>
2014-10-26 22:39:50 +0800

    update and bug fixes

--- database/examineDB.js
+++ database/examineDB.js
@@ -8,7 +8,63 @@ var examineDB = {};
  */
 examineDB.getAllExamArts = function (uid, callback) {
     //TODO
-    database.query("",callback);
+    database.query("", callback);
 };
+
+/**
+ * 评审某篇文章
+ * @param aid
+ * @param uid
+ * @param exam
+ * @param msg
+ * @param callback
+ */
+examineDB.examine = function (aid, uid, exam, msg, callback) {
+    //TODO
+    database.query("", callback);
+};
+
+/**
+ * 指定审稿人
+ * @param uid
+ * @param tid
+ * @param aid
+ * @param callback
+ */
+examineDB.send = function (uid, tid, aid, callback) {
+    //TODO
+    database.query("", callback);
+};
+
+/**
+ * 获取审稿历史
+ * @param aid
+ * @param callback
+ */
+examineDB.examHistory = function (aid, callback) {
+    //TODO
+    database.query("", callback);
+};
+
+/**
+ * 根据主编获取对应的审稿人信息
+ * @param uid
+ * @param callback
+ */
+examineDB.getExaminerList = function (uid, callback) {
+    //TODO
+    database.query("", callback);
+};
+
+/**
+ * 根据ID获取某篇文章的信息
+ * @param aid
+ * @param callback
+ */
+examineDB.getById = function (aid, callback) {
+    //TODO
+    database.query("", callback);
+};
+
 
 module.exports = examineDB;
--- routes/examine.js
+++ routes/examine.js
@@ -3,8 +3,6 @@
  */
 var express = require('express');
 var examineDB = require('../database/examineDB');
-var validator = require('validator');
-var async = require('async');
 var role_validator = require("../routes/role_validator");
 var result = require("../result");
 var router = express.Router();
@@ -28,11 +26,93 @@ router.get("examArts", role_validator, function (req, res) {
     });
 });
 
-
 /**
  * 审核文章
  */
-router.examine("examine", role_validator, function (req, res) {
+router.post("examine", role_validator, function (req, res) {
+    var uid = req.param("uid");
+    var aid = req.param("articleId");
+    var msg = req.param("comment");
+    var exam = req.param("status");
+    examineDB.examine(aid, uid, exam, msg, function (err, recordSet) {
+        if (!err) {
+            res.status(200).json(new result(false, null, "exmaine failed!"));
+        } else {
+            var data = [];
+            for (var i = 0; i < recordSet.length; i++) {
+                data[i] = recordSet[i];
+            }
+            res.status(200).json(new result(true, data, ""));
+        }
+    });
+});
 
+
+/**
+ * 获取审稿人信息
+ */
+router.get("examiner", role_validator, function (req, res) {
+    var uid = req.param("uid");
+    examineDB.getExaminerList(uid, function (err, recordSet) {
+        if (!err) {
+            res.status(200).json(new result(false, null, "get examiner info failed"));
+        } else {
+            var data = [];
+            for (var i = 0; i < recordSet.length; i++) {
+                data[i] = recordSet[i];
+            }
+            res.status(200).json(new result(true, data, ""));
+        }
+    });
+});
+
+/**
+ * 转交给别的审稿人
+ */
+router.get("forward", role_validator, function (req, res) {
+    var uid = req.param("uid");
+    var tid = req.param("tid");
+    var aids = req.param("articleIds");
+    examineDB.send(uid, tid, aids, function (err, recordSet) {
+        if (!err) {
+            res.status(200).json(new result(false, null, "forward failed"));
+        } else {
+            res.status(200).json(new result(true, null, null));
+        }
+    });
+});
+
+/**
+ * 获取某篇文章的审稿记录
+ */
+router.get("history", role_validator, function (req, res) {
+    var uid = req.param("uid");
+    var aid = req.param("articleId");
+    examineDB.examHistory(aid, function (err, recordSet) {
+        if (!err) {
+            res.status(200).json(new result(false, null, "get history failed"));
+        } else {
+            var data = [];
+            for (var i = 0; i < recordSet.length; i++) {
+                data[i] = recordSet[i];
+            }
+            res.status(200).json(new result(true, data, ""));
+        }
+    });
+});
+
+/**
+ * 读取文章信息
+ */
+router.get("get", role_validator, function (req, res) {
+    var aid = req.param("articleId");
+    examineDB.getById(aid, function (err, recordSet) {
+        if (!err) {
+            res.status(200).json(new result(false, null, "get article failed"));
+        } else {
+            var data = recordSet.length > 0 ? recordSet[0] : {};
+            res.status(200).json(new result(true, recordSet, ""));
+        }
+    });
 });
 module.exports = router;
